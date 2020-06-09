658535bd1d69ff73a51d438616ef53f8d6942a1d
mlazzell17@gmail.com <MicheSi>
2020-06-09 08:41:02 -0700

    completed dfs_recursive

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -177,7 +177,7 @@ class Graph:
                     s.push(neighbors)
 
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -185,7 +185,28 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass
+        # base case
+        if visited is None:
+            visited = set()
+
+        if path is None:
+            path = []
+
+        # track visited verts
+        visited.add(starting_vertex)
+        # make copy of the path
+        path = path + [starting_vertex]
+
+        if starting_vertex == destination_vertex:
+            return path
+
+        for neighbor in self.vertices[starting_vertex]:
+            if neighbor not in visited:
+                new_path = self.dfs_recursive(neighbor, destination_vertex, visited, path)
+                if new_path:
+                    return new_path
+
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
