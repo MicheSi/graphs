b08481f90be70476818662b3886785f8247b83bd
mlazzell17@gmail.com <MicheSi>
2020-06-09 18:35:57 -0700

    earliest ancestor working and passing tests!

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('../graph')
+from graph import Graph
+from util import Queue
+
 # data is list of (parent, child) pairs
 # returns earliest ancestor
 # if more than one at "earliest", return lowest numeric ID
@@ -5,35 +10,20 @@
 
 # create path of nodes (to get back to parent)
 # keep track of neighbors (to keep track of children)
-import sys
-sys.path.append('../graph')
-from graph import Graph
-from util import Queue
-
-
-# def get_neighbors(arr, node):
-#     # create empty list for neighbors
-#     neighbors = []
-
-#     # data = list(node)
-
-#     for n in range(len(arr)):
-#         if arr[n][1] == node:
-#             neighbors.append(arr[n][0])
-
-#     return neighbors
 
 def earliest_ancestor(ancestors, starting_node):
     # create empty Graph
     g = Graph()
 
     # for each tuple
-    for parent, child in ancestors:
+    for pair in ancestors:
         # add verts to graph
-        g.add_vertex(parent)
-        g.add_vertex(child)
-        # make directed by pointed child to parent
-        g.add_edge(child, parent)
+        g.add_vertex(pair[0]) # parent
+        g.add_vertex(pair[1]) # child
+    for pair in ancestors:
+        # make directed by pointing child to parent
+        g.add_edge(pair[1], pair[0])
+
 
     # create empty Queue
     q = Queue()
@@ -59,5 +49,26 @@ def earliest_ancestor(ancestors, starting_node):
                 new_path.append(parent)
                 # add copy of path
                 q.enqueue(new_path)
-    return vert
+        return vert
 
+
+    # max_path_len = 1
+    # earliest_ancestor = -1
+
+    # while q.size() > 0:
+    #     path = q.dequeue()
+    #     vert = path[-1]
+    #     # if path is longer or equal and value is smaller, or if path is longer
+    #     if (len(path) >= max_path_len and vert < earliest_ancestor) or (len(path) > max_path_len):
+    #         earliest_ancestor = vert
+    #         max_path_len = len(path)
+    #     for parent in g.vertices[vert]:
+    #         new_path = list(path)
+    #         new_path.append(parent)
+    #         q.enqueue(new_path)
+    # return earliest_ancestor
+
+
+if __name__ == '__main__':
+    test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+    print(earliest_ancestor(test_ancestors, 3))
